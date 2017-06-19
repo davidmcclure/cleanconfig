1a464f103c73420d8041373e88027167f971c598
David McClure <davidwilliammcclure@gmail.com>
2017-06-19 12:45:48 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='simpleconfig',
+    version='0.1.0',
+    description='Simple configuration for Python projects.',
+    author='David McClure',
+    author_email='dclure@mit.edu',
+    url='https://github.com/opensyllabus/osp-pipeline',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[
+        'anyconfig',
+        'voluptuous',
+    ],
+)
