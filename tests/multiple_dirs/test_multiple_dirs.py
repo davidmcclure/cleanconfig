82fc128c7bd9c0168db4513e40b972fa89c59985
David McClure <davidwilliammcclure@gmail.com>
2017-06-23 15:18:12 -0700

    Harden up multiple dirs test.

--- tests/multiple_dirs/test_multiple_dirs.py
+++ tests/multiple_dirs/test_multiple_dirs.py
@@ -15,13 +15,20 @@ class Config(CleanConfig):
     config_dirs = [
         fixture_path(__file__, 'dir1'),
         fixture_path(__file__, 'dir2'),
+        fixture_path(__file__, 'dir3'),
     ]
 
     schema = Schema({
-        Required('key'): str,
+        Required('key1'): int,
+        Required('key2'): int,
+        Required('key3'): int,
     })
 
 
 def test_multiple_dirs():
+
     config = Config.read()
-    assert config['key'] == 'val2'
+
+    assert config['key1'] == 1
+    assert config['key2'] == 2
+    assert config['key3'] == 3
